5e544de01ee90c991527528ce9cf1c05da4d46eb
xiaochun <90xchun@gmail.com>
2025-08-13 11:15:12 +0800

    Upgrade 3.x

--- src/main/java/com/zrlog/plugin/changyan/controller/ChangyanController.java
+++ src/main/java/com/zrlog/plugin/changyan/controller/ChangyanController.java
@@ -1,7 +1,6 @@
 package com.zrlog.plugin.changyan.controller;
 
 import com.google.gson.Gson;
-import com.zrlog.plugin.IMsgPacketCallBack;
 import com.zrlog.plugin.IOSession;
 import com.zrlog.plugin.changyan.response.ChangyanComment;
 import com.zrlog.plugin.changyan.response.CommentsEntry;
@@ -37,40 +36,35 @@ public class ChangyanController {
     }
 
     public void update() {
-        session.sendMsg(new MsgPacket(requestInfo.simpleParam(), ContentType.JSON, MsgPacketStatus.SEND_REQUEST, IdUtil.getInt(), ActionType.SET_WEBSITE.name()), new IMsgPacketCallBack() {
-            @Override
-            public void handler(MsgPacket msgPacket) {
-                Map<String, Object> map = new HashMap<>();
-                map.put("success", true);
-                session.sendMsg(new MsgPacket(map, ContentType.JSON, MsgPacketStatus.RESPONSE_SUCCESS, requestPacket.getMsgId(), requestPacket.getMethodStr()));
-            }
+        session.sendMsg(new MsgPacket(requestInfo.simpleParam(), ContentType.JSON, MsgPacketStatus.SEND_REQUEST, IdUtil.getInt(),
+                ActionType.SET_WEBSITE.name()), msgPacket -> {
+            Map<String, Object> map = new HashMap<>();
+            map.put("success", true);
+            session.sendMsg(new MsgPacket(map, ContentType.JSON, MsgPacketStatus.RESPONSE_SUCCESS, requestPacket.getMsgId(), requestPacket.getMethodStr()));
         });
     }
 
     public void index() {
         Map<String, Object> keyMap = new HashMap<>();
         keyMap.put("key", "appId,appKey,status,commentEmailNotify,callbackUrl");
-        session.sendJsonMsg(keyMap, ActionType.GET_WEBSITE.name(), IdUtil.getInt(), MsgPacketStatus.SEND_REQUEST, new IMsgPacketCallBack() {
-            @Override
-            public void handler(MsgPacket msgPacket) {
-                Map map = new Gson().fromJson(msgPacket.getDataStr(), Map.class);
-                map.put("userName", requestInfo.getUserName());
-                map.put("userId", requestInfo.getUserId());
-                map.put("fullUrl", requestInfo.getFullUrl().replace("install", ""));
-                if (map.get("callbackUrl") == null || "".equals(map.get("callbackUrl"))) {
-                    map.put("callbackUrl", requestInfo.getAccessUrl() + "/p/" + session.getPlugin().getShortName() + "/sync/" + UUID.randomUUID().toString().replace("-", ""));
-                }
-                if(!Objects.equals(map.get("status"),"on")){
-                    map.remove("status");
-                }
-                if(!Objects.equals(map.get("commentEmailNotify"),"on")){
-                    map.remove("commentEmailNotify");
-                }
-                Map<String, Object> data = new HashMap<>();
-                data.put("theme", Objects.equals(requestInfo.getHeader().get("Dark-Mode"), "true") ? "dark" : "light");
-                data.put("data", new Gson().toJson(map));
-                session.responseHtmlStr(new SimpleTemplateRender().render("/templates/index.html", session.getPlugin(), data), requestPacket.getMethodStr(), requestPacket.getMsgId());
+        session.sendJsonMsg(keyMap, ActionType.GET_WEBSITE.name(), IdUtil.getInt(), MsgPacketStatus.SEND_REQUEST, msgPacket -> {
+            Map map = new Gson().fromJson(msgPacket.getDataStr(), Map.class);
+            map.put("userName", requestInfo.getUserName());
+            map.put("userId", requestInfo.getUserId());
+            map.put("fullUrl", requestInfo.getFullUrl().replace("install", ""));
+            if (map.get("callbackUrl") == null || "".equals(map.get("callbackUrl"))) {
+                map.put("callbackUrl", requestInfo.getAccessUrl() + "/p/" + session.getPlugin().getShortName() + "/sync/" + UUID.randomUUID().toString().replace("-", ""));
             }
+            if (!Objects.equals(map.get("status"), "on")) {
+                map.remove("status");
+            }
+            if (!Objects.equals(map.get("commentEmailNotify"), "on")) {
+                map.remove("commentEmailNotify");
+            }
+            Map<String, Object> data = new HashMap<>();
+            data.put("theme", Objects.equals(requestInfo.getHeader().get("Dark-Mode"), "true") ? "dark" : "light");
+            data.put("data", new Gson().toJson(map));
+            session.responseHtmlStr(new SimpleTemplateRender().render("/templates/index", session.getPlugin(), data), requestPacket.getMethodStr(), requestPacket.getMsgId());
         });
 
     }
@@ -85,7 +79,7 @@ public class ChangyanController {
                 articleId = "-1";
             }
             map.put("articleId", articleId);
-            session.responseHtmlStr(new SimpleTemplateRender().render("/templates/widget.html", session.getPlugin(), map), requestPacket.getMethodStr(), requestPacket.getMsgId());
+            session.responseHtmlStr(new SimpleTemplateRender().render("/templates/widget", session.getPlugin(), map), requestPacket.getMethodStr(), requestPacket.getMsgId());
         });
 
     }
@@ -96,26 +90,23 @@ public class ChangyanController {
     public void sync() {
         Map<String, Object> keyMap = new HashMap<>();
         keyMap.put("key", "short_name,secret,status,commentEmailNotify,callbackUrl");
-        session.sendJsonMsg(keyMap, ActionType.GET_WEBSITE.name(), IdUtil.getInt(), MsgPacketStatus.SEND_REQUEST, new IMsgPacketCallBack() {
-            @Override
-            public void handler(MsgPacket msgPacket) {
-                Map<String, Object> changyan = new Gson().fromJson(msgPacket.getDataStr(), Map.class);
-                String callbackUrl = (String) changyan.get("callbackUrl");
-                String ignoreChar = "/p" + "/" + session.getPlugin().getShortName();
-                try {
-                    if (callbackUrl != null && new URL(callbackUrl).getPath().replace(ignoreChar, "").equals(requestInfo.getUri().replace(".action", ""))) {
-                        String commentJsonStr = requestInfo.getParam().get("data")[0];
-                        LOGGER.info(commentJsonStr);
-                        final ChangyanComment changyanComment = new Gson().fromJson(commentJsonStr, ChangyanComment.class);
-                        Map<String, Object> response = new HashMap<>();
-                        dealSyncRequest(response, changyanComment, "on".equals(changyan.get("commentEmailNotify")));
-                    } else {
-                        session.sendMsg(ContentType.HTML, ClientActionHandler.ACTION_NOT_FOUND_PAGE, requestPacket.getMethodStr(), requestPacket.getMsgId(), MsgPacketStatus.RESPONSE_ERROR);
-                    }
-                } catch (Exception e) {
-                    LOGGER.log(Level.SEVERE, "", e);
-                    session.sendMsg(ContentType.HTML, "Exception", requestPacket.getMethodStr(), requestPacket.getMsgId(), MsgPacketStatus.RESPONSE_ERROR);
+        session.sendJsonMsg(keyMap, ActionType.GET_WEBSITE.name(), IdUtil.getInt(), MsgPacketStatus.SEND_REQUEST, msgPacket -> {
+            Map<String, Object> changyan = new Gson().fromJson(msgPacket.getDataStr(), Map.class);
+            String callbackUrl = (String) changyan.get("callbackUrl");
+            String ignoreChar = "/p" + "/" + session.getPlugin().getShortName();
+            try {
+                if (callbackUrl != null && new URL(callbackUrl).getPath().replace(ignoreChar, "").equals(requestInfo.getUri().replace(".action", ""))) {
+                    String commentJsonStr = requestInfo.getParam().get("data")[0];
+                    LOGGER.info(commentJsonStr);
+                    final ChangyanComment changyanComment = new Gson().fromJson(commentJsonStr, ChangyanComment.class);
+                    Map<String, Object> response = new HashMap<>();
+                    dealSyncRequest(response, changyanComment, "on".equals(changyan.get("commentEmailNotify")));
+                } else {
+                    session.sendMsg(ContentType.HTML, ClientActionHandler.ACTION_NOT_FOUND_PAGE, requestPacket.getMethodStr(), requestPacket.getMsgId(), MsgPacketStatus.RESPONSE_ERROR);
                 }
+            } catch (Exception e) {
+                LOGGER.log(Level.SEVERE, "", e);
+                session.sendMsg(ContentType.HTML, "Exception", requestPacket.getMethodStr(), requestPacket.getMsgId(), MsgPacketStatus.RESPONSE_ERROR);
             }
         });
 
@@ -128,29 +119,23 @@ public class ChangyanController {
                 final Comment comment = getComment(changyanComment, commentsEntry);
 
                 LOGGER.log(Level.INFO, "changyan call " + new Gson().toJson(comment));
-                session.sendMsg(ContentType.JSON, comment, ActionType.ADD_COMMENT.name(), IdUtil.getInt(), MsgPacketStatus.SEND_REQUEST, new IMsgPacketCallBack() {
-                    @Override
-                    public void handler(MsgPacket msgPacket) {
-                        response.put("status", msgPacket.getStatus() == MsgPacketStatus.RESPONSE_SUCCESS ? 200 : 500);
-                        session.sendMsg(ContentType.JSON, response, requestPacket.getMethodStr(), requestPacket.getMsgId(), MsgPacketStatus.RESPONSE_SUCCESS);
-                    }
+                session.sendMsg(ContentType.JSON, comment, ActionType.ADD_COMMENT.name(), IdUtil.getInt(), MsgPacketStatus.SEND_REQUEST, msgPacket -> {
+                    response.put("status", msgPacket.getStatus() == MsgPacketStatus.RESPONSE_SUCCESS ? 200 : 500);
+                    session.sendMsg(ContentType.JSON, response, requestPacket.getMethodStr(), requestPacket.getMsgId(), MsgPacketStatus.RESPONSE_SUCCESS);
                 });
                 if (emailNotify) {
-                    session.sendMsg(ContentType.JSON, new HashMap<>(), ActionType.LOAD_PUBLIC_INFO.name(), IdUtil.getInt(), MsgPacketStatus.SEND_REQUEST, new IMsgPacketCallBack() {
-                        @Override
-                        public void handler(MsgPacket msgPacket) {
-                            PublicInfo publicInfo = msgPacket.convertToClass(PublicInfo.class);
-                            Map<String, String> map = new HashMap<>();
-                            Map<String, Object> moduleMap = new HashMap<>();
-                            moduleMap.put("content", comment.getContent());
-                            moduleMap.put("title", changyanComment.getTitle());
-                            moduleMap.put("titleUrl", changyanComment.getUrl());
-                            moduleMap.put("username", comment.getName());
-                            moduleMap.put("version", session.getPlugin().getVersion());
-                            map.put("content", new SimpleTemplateRender().render("/email/notify-email.html", session.getPlugin(), moduleMap));
-                            map.put("title", publicInfo.getTitle() + " 有了新的评论");
-                            session.requestService("emailService", map);
-                        }
+                    session.sendMsg(ContentType.JSON, new HashMap<>(), ActionType.LOAD_PUBLIC_INFO.name(), IdUtil.getInt(), MsgPacketStatus.SEND_REQUEST, msgPacket -> {
+                        PublicInfo publicInfo = msgPacket.convertToClass(PublicInfo.class);
+                        Map<String, String> map = new HashMap<>();
+                        Map<String, Object> moduleMap = new HashMap<>();
+                        moduleMap.put("content", comment.getContent());
+                        moduleMap.put("title", changyanComment.getTitle());
+                        moduleMap.put("titleUrl", changyanComment.getUrl());
+                        moduleMap.put("username", comment.getName());
+                        moduleMap.put("version", session.getPlugin().getVersion());
+                        map.put("content", new SimpleTemplateRender().render("/email/notify-email.html", session.getPlugin(), moduleMap));
+                        map.put("title", publicInfo.getTitle() + " 有了新的评论");
+                        session.requestService("emailService", map);
                     });
                 }
             }
