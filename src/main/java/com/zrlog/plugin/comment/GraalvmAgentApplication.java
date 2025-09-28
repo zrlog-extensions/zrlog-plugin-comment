67e6cedeabad2943cc1a06dec5f89a799b1f311c
xiaochun <90xchun@gmail.com>
2025-09-28 14:05:14 +0800

    Optimize style

--- src/main/java/com/zrlog/plugin/comment/GraalvmAgentApplication.java
+++ src/main/java/com/zrlog/plugin/comment/GraalvmAgentApplication.java
@@ -6,8 +6,6 @@ import com.zrlog.plugin.comment.response.ChangyanComment;
 import com.zrlog.plugin.comment.response.CommentsEntry;
 import com.zrlog.plugin.comment.response.User;
 import com.zrlog.plugin.common.PluginNativeImageUtils;
-import com.zrlog.plugin.data.codec.HttpRequestInfo;
-import com.zrlog.plugin.message.Plugin;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,15 +15,14 @@ public class GraalvmAgentApplication {
 
 
     public static void main(String[] args) throws IOException {
-        new Gson().toJson(new HttpRequestInfo());
-        new Gson().toJson(new Plugin());
-        new Gson().toJson(new User());
+        PluginNativeImageUtils.usedGsonObject();
         new Gson().toJson(new ChangyanComment());
         new Gson().toJson(new CommentsEntry());
-        String basePath = System.getProperty("user.dir").replace("\\target","").replace("/target", "");
+        new Gson().toJson(new User());
+        String basePath = System.getProperty("user.dir").replace("\\target", "").replace("/target", "");
         //PathKit.setRootPath(basePath);
         File file = new File(basePath + "/src/main/resources");
-        PluginNativeImageUtils.doLoopResourceLoad(file.listFiles(), file.getPath()  + "/", "/");
+        PluginNativeImageUtils.doLoopResourceLoad(file.listFiles(), file.getPath() + "/", "/");
         //Application.nativeAgent = true;
         PluginNativeImageUtils.exposeController(Collections.singletonList(CommentController.class));
         Application.main(args);
