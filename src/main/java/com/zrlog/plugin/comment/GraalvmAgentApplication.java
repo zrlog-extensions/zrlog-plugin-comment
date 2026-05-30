2cb41878e37c7def9661585d090dc15ed034be58
xiaochun <90xchun@gmail.com>
2026-05-30 10:52:16 +0800

    update package shell

--- src/main/java/com/zrlog/plugin/comment/GraalvmAgentApplication.java
+++ src/main/java/com/zrlog/plugin/comment/GraalvmAgentApplication.java
@@ -1,6 +1,5 @@
 package com.zrlog.plugin.comment;
 
-import com.google.gson.Gson;
 import com.zrlog.plugin.comment.controller.CommentController;
 import com.zrlog.plugin.comment.response.ChangyanComment;
 import com.zrlog.plugin.comment.response.CommentsEntry;
@@ -9,6 +8,7 @@ import com.zrlog.plugin.common.PluginNativeImageUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class GraalvmAgentApplication {
@@ -16,9 +16,7 @@ public class GraalvmAgentApplication {
 
     public static void main(String[] args) throws IOException {
         PluginNativeImageUtils.usedGsonObject();
-        new Gson().toJson(new ChangyanComment());
-        new Gson().toJson(new CommentsEntry());
-        new Gson().toJson(new User());
+        PluginNativeImageUtils.gsonNativeAgentByClazz(Arrays.asList(ChangyanComment.class, CommentsEntry.class, User.class));
         String basePath = System.getProperty("user.dir").replace("\\target", "").replace("/target", "");
         //PathKit.setRootPath(basePath);
         File file = new File(basePath + "/src/main/resources");
